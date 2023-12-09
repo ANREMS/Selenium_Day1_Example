220cc4772a03c1b3f9b46372f3fe30b38cf535eb
Annadurai <choannadurai@gmail.com>
2023-12-09 15:18:11 +0000

    Code comitted to GIT

---
+++ src/test/java/Class3.java
@@ -0,0 +1,9 @@
+public class Class3 {
+    void multiplication (){
+        System.out.println("Multiplication");
+    }
+
+    void division (){
+        System.out.println("Division");
+    }
+}
---
+++ src/test/java/Class4.java
@@ -0,0 +1,16 @@
+public class Class4 {
+
+    void method1(){
+        System.out.println("Method1");
+    }
+
+    void method2(){
+        System.out.println("Method2");
+    }
+    public static void main(String[] args) {
+        System.out.println("Test program for GIT Hub");
+
+    }
+
+
+}
